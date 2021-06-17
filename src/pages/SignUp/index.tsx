45be21ef1e993c149a8004d7fabf17d3528fbda4
Edgar Justavino <edgarja_26@hotmail.com>
2021-06-16 23:53:16 -0300

    App

---
+++ src/pages/SignUp/index.tsx
@@ -0,0 +1,156 @@
+import React, { useCallback, useRef } from 'react';
+import {
+    Image,
+    View,
+    ScrollView,
+    KeyboardAvoidingView,
+    Platform,
+    TextInput,
+    Alert,
+} from 'react-native';
+import Icon from 'react-native-vector-icons/Feather';
+import { useNavigation } from '@react-navigation/native';
+import { Form } from '@unform/mobile';
+import { FormHandles } from '@unform/core';
+import * as Yup from 'yup';
+import Input from '../../components/Input';
+import Button from '../../components/Button';
+import logoImg from '../../assets/logo.png';
+import getValidationErrors from '../../utils/getValidationErrors';
+import api from '../../services/api';
+import { Container, Title, BackToSignIn, BackToSignInText } from './styles';
+
+interface SignUpFormData {
+    name: string;
+    email: string;
+    password: string;
+}
+
+const SignUp: React.FC = () => {
+    const formRef = useRef<FormHandles>(null);
+    const emailInputRef = useRef<TextInput>(null);
+    const passwordInputRef = useRef<TextInput>(null);
+    const navigation = useNavigation();
+    const handleSignUp = useCallback(
+        async (data: SignUpFormData) => {
+            try {
+                formRef.current?.setErrors({});
+
+                const schema = Yup.object().shape({
+                    name: Yup.string().required('Nome obrigatório'),
+                    email: Yup.string()
+                        .required('E-mail obrigatório')
+                        .email('Digite um e-mail válido'),
+                    password: Yup.string()
+                        .required()
+                        .min(6, 'No mínimo 6 dígitos'),
+                });
+
+                await schema.validate(data, {
+                    abortEarly: false,
+                });
+
+                await api.post('/users', data);
+
+                Alert.alert(
+                    'Cadastro realizado com sucesso!',
+                    'Você já pode fazer login na aplicação.',
+                );
+
+                navigation.goBack();
+            } catch (err) {
+                if (err instanceof Yup.ValidationError) {
+                    const errors = getValidationErrors(err);
+
+                    formRef.current?.setErrors(errors);
+
+                    return;
+                }
+
+                Alert.alert(
+                    'Cadastro realizado',
+                    'Você jà pode fazer logon no GoBarber!',
+                );
+            }
+        },
+        [navigation],
+    );
+
+    return (
+        <>
+            <KeyboardAvoidingView
+                style={{ flex: 1 }}
+                behavior={Platform.OS === 'ios' ? 'padding' : undefined}
+                enabled
+            >
+                <ScrollView
+                    keyboardShouldPersistTaps="handled"
+                    contentContainerStyle={{ flex: 1 }}
+                >
+                    <Container>
+                        <Image source={logoImg} />
+
+                        <View>
+                            <Title>Crie sua conta</Title>
+                        </View>
+
+                        <Form ref={formRef} onSubmit={handleSignUp}>
+                            <Input
+                                autoCapitalize="words"
+                                returnKeyType="next"
+                                onSubmitEditing={() => {
+                                    emailInputRef.current?.focus();
+                                }}
+                                name="name"
+                                icon="user"
+                                placeholder="Nome"
+                            />
+                            <Input
+                                ref={emailInputRef}
+                                keyboardType="email-address"
+                                autoCapitalize="none"
+                                autoCorrect={false}
+                                returnKeyType="next"
+                                onSubmitEditing={() => {
+                                    passwordInputRef.current?.focus();
+                                }}
+                                name="email"
+                                icon="mail"
+                                placeholder="E-mail"
+                            />
+                            <Input
+                                ref={passwordInputRef}
+                                secureTextEntry
+                                textContentType="newPassword"
+                                returnKeyType="send"
+                                onSubmitEditing={() =>
+                                    formRef.current?.submitForm()
+                                }
+                                name="password"
+                                icon="lock"
+                                placeholder="Senha"
+                            />
+
+                            <Button
+                                onPress={() => formRef.current?.submitForm()}
+                            >
+                                Cadastrar
+                            </Button>
+                        </Form>
+                    </Container>
+                </ScrollView>
+            </KeyboardAvoidingView>
+
+            <BackToSignIn
+                onPress={() => {
+                    navigation.goBack();
+                }}
+            >
+                <Icon name="arrow-left" size={20} color="#fff" />
+                <BackToSignInText>Voltar para login</BackToSignInText>
+            </BackToSignIn>
+        </>
+    );
+};
+
+export default SignUp;
